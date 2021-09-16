e3ca78b04d5ade6de46eeafbcb132727e8392360
gnessdo <87198355+gnessdo@users.noreply.github.com>
2021-09-16 18:32:14 +0400

    Add files via upload

---
+++ 2/script.js
@@ -0,0 +1,20 @@
+
+
+
+function musteri () {
+
+    let eded = document.getElementById("num").Value;
+    let musteri = "";
+
+for( let i = 1; i<=eded; i++) {
+
+document.getElementById (`<input type="number" class="form-control" id="num">`).innerHTML = musteri
+
+document.getElementById ("show").
+
+}
+
+
+}
+
+musteri ();
